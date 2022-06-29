8bf518829324484d0a8c0dd2ac8d3b7759f5d153
thetazero <lesha.seletskiy@gmail.com>
2022-06-29 12:59:31 -0400

    Detumbling imports compatible with both numpy/ulab

--- state_machine/applications/flight/Tasks/detumble.py
+++ state_machine/applications/flight/Tasks/detumble.py
@@ -1,27 +1,30 @@
-from numpy import identity, asarray, linalg, cross, matmul, atleast_2d, isnan, any
+try:
+    from ulab.numpy import eye as identity, array, linalg, cross, dot as matmul, isfinite, all
+except Exception:
+    from numpy import identity, array, linalg, cross, matmul, isfinite, all
 
 from lib.template_task import Task
 
 def bcross(b, ω, k=7e-4):
-    b = (asarray(b))
-    ω = (asarray(ω))
+    b = (array(b))
+    ω = (array(ω))
     b_hat = b / linalg.norm(b)
-    bbt = matmul(atleast_2d(b_hat).T, atleast_2d(b_hat))
+    bbt = matmul(array([b_hat]).transpose(), array([b_hat]))
     M = - k * matmul(identity(3) - bbt, ω)
     # control
     m = 1 / (linalg.norm(b)) * (cross(b_hat, M))
-    if any(isnan(m)):
-        return [0, 0, 0]
-    return m
+    if all(isfinite(m)):
+        return m
+    return [0, 0, 0]
 
 class task(Task):
-    name = 'detubmle'
+    name = 'detumble'
     color = 'pink'
 
     rgb_on = False
 
     async def main_task(self):
-        b = (asarray(self.cubesat.magnetic))
+        b = (array(self.cubesat.magnetic))
         m = bcross(self.cubesat.magnetic, self.cubesat.gyro)
 
         self.debug(f'Detumbling with magnetorquer set to {m}')
