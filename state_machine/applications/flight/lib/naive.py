d255726c5027fc8c6f2686a6863da17dcc376861
thetazero <lesha.seletskiy@gmail.com>
2022-07-06 10:42:34 -0400

    Explain packet length for naive transmission

--- state_machine/applications/flight/lib/naive.py
+++ state_machine/applications/flight/lib/naive.py
@@ -5,12 +5,13 @@ class NaiveMessage(Message):
     def __init__(self, priority, str):
         super().__init__(priority, str)
         self.cursor = 0
+        self.packet_len = 250  # not 252 because for some reason packet loss is extremely high at this threshold
 
     def packet(self):
-        return self.str[self.cursor:self.cursor + 250], True
+        return self.str[self.cursor:self.cursor + self.packet_len], True
 
     def done(self):
         return len(self.str) == self.cursor
 
     def ack(self):
-        self.cursor += 250
+        self.cursor += self.packet_len
