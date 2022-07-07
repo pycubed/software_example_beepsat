09d9cd27eb9de42f7ad9861f71762e78429866ac
thetazero <lesha.seletskiy@gmail.com>
2022-07-07 11:06:15 -0400

    Remove test tasks from flight application

---
+++ state_machine/applications/flight/Tasks/mekf.py
@@ -0,0 +1,10 @@
+from lib.template_task import Task
+
+
+class task(Task):
+    """Multiplicative Extended Kalman Filter"""
+    name = 'mekf'
+    color = 'red'
+
+    async def main_task(self):
+        pass
