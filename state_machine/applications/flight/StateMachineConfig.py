11c7279e52dec679f6cc5f31d7740ba9a26fc98f
thetazero <lesha.seletskiy@gmail.com>
2022-07-07 10:19:23 -0400

    Set Detumble task to same interval as MEKF

--- state_machine/applications/flight/StateMachineConfig.py
+++ state_machine/applications/flight/StateMachineConfig.py
@@ -62,7 +62,7 @@ config = {
                 'ScheduleLater': False
             },
             'Detumble': {
-                'Interval': 5.0,
+                'Interval': 0.1,
                 'Priority': 3,
                 'ScheduleLater': False
             },
