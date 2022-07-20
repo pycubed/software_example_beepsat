966f149bc2247c22983f2d70a82609fdc6d519be
thetazero <lesha.seletskiy@gmail.com>
2022-07-20 09:14:19 -0400

    Termination header

--- state_machine/applications/flight/lib/naive.py
+++ state_machine/applications/flight/lib/naive.py
@@ -6,12 +6,14 @@ class NaiveMessage(Message):
         super().__init__(priority, str)
         self.cursor = 0
         self.header = 0xff
-        self.packet_len = 249  # not 252 because for some reason packet loss is extremely high at this threshold
+        self.packet_len = 249  # not 251 because for some reason packet loss is extremely high at this threshold
 
     def packet(self):
         str = self.str[self.cursor:self.cursor + self.packet_len]
         pkt = bytearray(len(str) + 1)
         pkt[0] = self.header
+        if len(self.str) <= self.cursor + self.packet_len:  # last packet
+            self.header = 0xfe
         pkt[1:] = self.str
         return pkt, True
 
