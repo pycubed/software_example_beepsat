a62df340ca01ed4215c45998030a6104b2835f53
thetazero <lesha.seletskiy@gmail.com>
2022-07-22 09:35:07 -0400

    Correctly test naive message

--- state_machine/unit_tests/test_naive.py
+++ state_machine/unit_tests/test_naive.py
@@ -1,20 +1,23 @@
-from lib.naive import NaiveMessage
 import unittest
 import sys
 
 sys.path.insert(0, './state_machine/applications/flight')
 
+from lib.naive import NaiveMessage
 
 class NaiveMessageTests(unittest.TestCase):
 
     def test(self):
         p = NaiveMessage(0, 'x' * 249)
-        self.assertEqual(p.packet()[0], 0xfe)
+        res, _ = p.packet()
+        self.assertEqual(res[0], 0xfe)
         p.ack()
         self.assertTrue(p.done())
         p = NaiveMessage(0, 'x' * 250)
-        self.assertEqual(p.packet()[0], 0xff)
+        res, _ = p.packet()
+        self.assertEqual(res[0], 0xff)
         p.ack()
-        self.assertEqual(p.packet()[0], 0xfe)
+        res, _ = p.packet()
+        self.assertEqual(res[0], 0xfe)
         p.ack()
         self.assertTrue(p.done())
