980f72460f11398ec1bef9cbf6bc126dd8cdb24b
thetazero <lesha.seletskiy@gmail.com>
2022-07-25 10:49:36 -0400

    placing undefined functions in the right spot

--- state_machine/applications/flight/lib/frames.py
+++ state_machine/applications/flight/lib/frames.py
@@ -1,12 +1,27 @@
+try:
+    import ulab.numpy as np
+except ImportError:
+    import numpy as np
+
 def eci_to_ecef(date):
     """Returns the transformation matrix from ECI (Earth Centered Inertial) to ECEF (Earth Centered Earth Fixed).
+    Applies corrections for bias, precession, nutation, Earth-rotation, and polar motion.
+
+    The transformation is accomplished using the IAU 2006/2000A, CIO-based
+    theory using classical angles. The method as described in section 5.5 of
+    the SOFA C transformation cookbook.
+
     Args:
         - date: A unix timestamp.
 
     Returns:
         - A 3x3 numpy array.
+    Based on: https://github.com/sisl/SatelliteDynamics.jl/blob/f1eede2faffd2d6a6864d7ac0989a075c7d7a04f/src/reference_systems.jl#L296
     """
-    return None
+    rc2i = bias_precession_nutation(date)
+    r = earth_rotation(date)
+    rpm = polar_motion(date)
+    return np.dot(rc2i, np.dot(r, rpm))
 
 def ecef_to_eci(date):
     """Returns the transformation matrix from ECEF (Earth Centered Earth Fixed) to ECI (Earth Centered Inertial).
