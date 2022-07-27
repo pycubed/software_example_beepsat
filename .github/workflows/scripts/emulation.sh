dc2e645ce44f4062f9d4d0ccf2a45f9cb712c0ef
thetazero <lesha.seletskiy@gmail.com>
2022-07-27 09:53:37 -0400

    Fix exit codes

--- .github/workflows/scripts/emulation.sh
+++ .github/workflows/scripts/emulation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd state_machine
 timeout 5s sh run.sh
-if $? == "124" then
+if [ $? == "124" ]; then
+    echo "Emulated flight software did not crash"
     exit 0
 fi
 echo "Emulated flight software crashed"
