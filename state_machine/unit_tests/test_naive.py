88af4a63dfbc83c03fb31f55f76a26fdfd8b9bb3
thetazero <lesha.seletskiy@gmail.com>
2022-07-29 11:26:17 -0400

    Update unit tests for NaiveMessage

--- state_machine/unit_tests/test_naive.py
+++ state_machine/unit_tests/test_naive.py
@@ -1,23 +1,41 @@
+from struct import pack
 import unittest
 import sys
 
 sys.path.insert(0, './state_machine/applications/flight')
 
 from lib.naive import NaiveMessage
+import lib.radio_headers as headers
 
 class NaiveMessageTests(unittest.TestCase):
 
     def test(self):
-        p = NaiveMessage(0, 'x' * 249)
+        packet_len = 249
+
+        p = NaiveMessage(0, 'x' * packet_len)
         res, _ = p.packet()
-        self.assertEqual(res[0], 0xfe)
+        self.assertEqual(res[0], headers.NAIVE_END)
         p.ack()
         self.assertTrue(p.done())
-        p = NaiveMessage(0, 'x' * 250)
+
+        p = NaiveMessage(0, 'x' * (packet_len + 1))
         res, _ = p.packet()
-        self.assertEqual(res[0], 0xff)
+        self.assertEqual(res[0], headers.NAIVE_START)
         p.ack()
         res, _ = p.packet()
-        self.assertEqual(res[0], 0xfe)
+        self.assertEqual(res[0], headers.NAIVE_END)
+        p.ack()
+        self.assertTrue(p.done())
+
+        p = NaiveMessage(0, 'x' * (packet_len * 2 + 1))
+        res, _ = p.packet()
+        self.assertEqual(res[0], headers.NAIVE_START)
+        p.ack()
+        res, _ = p.packet()
+        self.assertEqual(res[0], headers.NAIVE_MID)
+        p.ack()
+        self.assertFalse(p.done())
+        res, _ = p.packet()
+        self.assertEqual(res[0], headers.NAIVE_END)
         p.ack()
         self.assertTrue(p.done())
