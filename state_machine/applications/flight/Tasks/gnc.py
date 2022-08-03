aaded5faf77b581265c795e27a9990dec60b265f
thetazero <lesha.seletskiy@gmail.com>
2022-08-03 14:58:00 -0400

    Propogate position

--- state_machine/applications/flight/Tasks/gnc.py
+++ state_machine/applications/flight/Tasks/gnc.py
@@ -3,8 +3,13 @@ from lib.control import bcross
 import lib.pycubed as cubesat
 from lib.IGRF import igrf_eci
 from lib.sun_position import approx_sun_position_ECI
+import lib.orbital_mechanics as orbital_mechanics
 import lib.mekf as mekf
 import time
+try:
+    from ulab.numpy import array
+except ImportError:
+    from numpy import array
 
 
 def toStr(arr):
@@ -16,6 +21,7 @@ class task(Task):
 
     rgb_on = False
     last = None
+    r_eci = array([6871, -6571, -7071])
 
     async def main_task(self):
         if self.last is None:
@@ -25,12 +31,16 @@ class task(Task):
         t = time.monotonic()
         delta_t = t - self.last
 
+        # update mekf
         w = cubesat.gyro()
         br_mag = cubesat.magnetic()
         br_sun = cubesat.sun_vector()
-        nr_mag = igrf_eci(t, r_eci)
+        nr_mag = igrf_eci(t, self.r_eci)
         nr_sun = approx_sun_position_ECI(t)
         mekf.step(w, delta_t, nr_mag, nr_sun, br_mag, br_sun)
+
+        # propogate ECI position
+        self.r_eci = orbital_mechanics.propogate(self.r_eci, delta_t, integration_step=5)
 
         m = bcross(cubesat.magnetic(), cubesat.gyro())
 
