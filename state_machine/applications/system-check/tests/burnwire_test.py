2e2ae899cb108a6949fd58e2573aa44414b851fa
yashika-batra <56561968+yashika-batra@users.noreply.github.com>
2022-08-04 22:07:41 -0700

    style, formatting, documentation

--- state_machine/applications/system-check/tests/burnwire_test.py
+++ state_machine/applications/system-check/tests/burnwire_test.py
@@ -10,21 +10,21 @@ from lib import pycubed as cubesat
 
 def get_user_input():
     """
-    All user interaction happens in this function
-    Prompt users, get user inputs for voltage level and duration and return
+    Ask the user to type y/any key to start/cancel the test
+    If y, return user inputs for voltage level and duration
+    If any other key, return  None
     """
 
     # conduct the test
     print("Initializing burnwire test...")
     print("You can test using a multimeter or fishing line.")
-    print("If using a multimeter, please read the voltage between the" +
-          "ground pin (GND) and burnwire pin on the -Z solar board.")
+    print("""If using a multimeter, please read the voltage between the ground pin 
+(GND) and burnwire pin on the -Z solar board.""")
 
     # get user input for voltage level and duration
-    voltage = float(input(
-        "At what voltage do you want to run the burnwire IC? (up to 3.3V): "))
-    burn_time = int(input(
-        "For how long do you want to run the burnwire test? "))
+    voltage = float(input("""At what voltage do you want to run the burnwire IC?
+(number between 0 and 3.3): """))
+    burn_time = int(input("How long do you want to run the burnwire test? (seconds): "))
 
     start_test = input("Type Y to start the test, any key to cancel: ")
     if start_test.lower() != "y":
@@ -35,9 +35,9 @@ def get_user_input():
 
 def burnwire_test(result_dict, burnnum):
     """
-    All automation happens in this function
-    Select burnwire and voltage level
-    Process user test results and update the result dictionary accordingly
+    Retrieve burnwire and voltage level from get_user_input() and run the test
+    If None, update the result dictionary that the test was not run
+    Else, process user inputted test results and update the result dictionary
     """
 
     # get user input for voltage and duration
@@ -45,7 +45,7 @@ def burnwire_test(result_dict, burnnum):
 
     # if voltage and burn_time are None, user entered "N" to cancel the test
     if voltage is None and burn_time is None:
-        result_dict[f'Burnwire{burnnum}'] = ('Test was not run.', False)
+        result_dict[f"Burnwire{burnnum}"] = ("Test was not run.", None)
         return result_dict
 
     # else, user entered "Y", calculate voltage level and conduct the test
@@ -61,28 +61,31 @@ def burnwire_test(result_dict, burnnum):
         success = True
 
     # process results
-    result_key = f'Burnwire {burnnum}'
-    result_val_string = f'Tested burnwire {burnnum} at {voltage} V'
+    result_key = f"Burnwire {burnnum}"
+    result_val_string = f"Tested burnwire {burnnum} at {voltage} V"
     result_dict[result_key] = (result_val_string, success)
     return result_dict
 
 
 def run(hardware_dict, result_dict):
     """
-    Check that the correct hardware is initialized and run tests
+    Check burnwire 1 and 2
+    If initialized correctly, run test and update result dictionary
+    through burnwire_test
+    If not initialized, update result dictionary
     """
 
     # if no Burn Wire 1 detected, update result dictionary
-    if not hardware_dict['Burnwire1']:
-        result_dict['Burnwire1'] = ('No burnwire 1 detected', False)
+    if not hardware_dict["Burnwire1"]:
+        result_dict["Burnwire1"] = ("No burnwire 1 detected", None)
     else:  # Burn Wire 1 detected, run tests
         print("Starting Burnwire1 test...")
         burnwire_test(result_dict, 1)
         print("Burnwire1 Test complete.\n")
 
     # if no Burn Wire 2 detected, update result dictionary
-    if not hardware_dict['Burnwire2']:
-        result_dict['Burnwire2'] = ('No burnwire 2 detected', False)
+    if not hardware_dict["Burnwire2"]:
+        result_dict["Burnwire2"] = ("No burnwire 2 detected", None)
     else:  # Burn Wire 2 detected, run tests
         print("Starting Burnwire2 test...")
         burnwire_test(result_dict, 2)
