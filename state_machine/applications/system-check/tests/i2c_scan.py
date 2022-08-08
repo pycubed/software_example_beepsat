255ca47a4f1183886327fd9bd0a838c5c710e069
yashikabatra15 <56561968+yashikabatra15@users.noreply.github.com>
2022-08-07 22:54:17 -0700

    Revert "merged wrong way"
    This reverts commit 11fb242b17435f5a49244814b1a7ca91fe85dab1.

---
+++ state_machine/applications/system-check/tests/i2c_scan.py
@@ -0,0 +1,27 @@
+from lib.pycubed import _cubesat as cubesat
+
+
+def run():
+    """
+    For each i2c device, print addresses of connected devices
+    """
+    print("Starting I2C Scan...")
+    while not cubesat.i2c1.try_lock():
+        pass
+    print("Addresses of Devices Connected to I2C1:",
+          [hex(x) for x in cubesat.i2c1.scan()])
+    cubesat.i2c1.unlock()
+
+    while not cubesat.i2c2.try_lock():
+        pass
+    print("Addresses of Devices Connected to I2C2:",
+          [hex(x) for x in cubesat.i2c2.scan()])
+    cubesat.i2c2.unlock()
+
+    while not cubesat.i2c3.try_lock():
+        pass
+    print("Addresses of Devices Connected to I2C3:",
+          [hex(x) for x in cubesat.i2c3.scan()])
+    cubesat.i2c3.unlock()
+
+    print("I2C Scan Complete.\n")
