de965ff4124aa51a69ff26872b52bb5966f83704
yashika-batra <56561968+yashika-batra@users.noreply.github.com>
2022-08-11 12:02:28 -0700

    fix comments, add docstring

--- state_machine/applications/system-check/tests/logging_infrastructure_test.py
+++ state_machine/applications/system-check/tests/logging_infrastructure_test.py
@@ -3,32 +3,37 @@ from os import listdir
 
 
 def run(hardware_dict, result_dict):
+    """
+    check that the SD Card is initialized and test the logging infrastructure
+    log multiple files' worth of content, and check that the correct filenames
+    and correct number of files were created
+    """
 
-    # if no SD Card detected, update result dictionary and return
+    # check that the SD card is initialized
     if not hardware_dict["SDcard"]:
         result_dict["LoggingInfrastructure_Test"] = (
             "Cannot test Logging Infrastructure; no SD Card detected", None)
         return result_dict
 
-    # else, clear storage and run test
     print("Starting logging infrastructure test...")
     clear_storage()
     print(f"/sd/ directory: {listdir('/sd/')}")
 
+    # log 1900 characters
     for i in range(18):
         msg = 'x' * 100
         log(msg, 1000)
-
+    
+    # check that log000.txt and log001.txt were created
     logfile1 = get_logfile_name(0)
     logfile2 = get_logfile_name(1)
-
-    # should create 2 files
     logfile1_created = logfile1 in listdir("/sd/logs/")
     logfile2_created = logfile2 in listdir("/sd/logs/")
 
     print(f"/sd/ directory: {listdir('/sd/')}")
     print(f"/sd/logs/ directory: {listdir('/sd/logs/')}")
-
+    
+    # update result dictionary
     if logfile1_created and logfile2_created:
         result_string = """Log and new log functions are working;
 files were created successfully."""
@@ -40,5 +45,4 @@ files were created successfully."""
         print(result_string)
 
     print("Logging Infrastructure Test complete.\n")
-
     clear_storage()
