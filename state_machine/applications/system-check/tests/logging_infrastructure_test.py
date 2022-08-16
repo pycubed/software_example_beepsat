3301395119552f4e5db9df5f463735fb7f194f3a
yashika-batra <56561968+yashika-batra@users.noreply.github.com>
2022-08-16 00:45:04 -0700

    comment/style fixes

--- state_machine/applications/system-check/tests/logging_infrastructure_test.py
+++ state_machine/applications/system-check/tests/logging_infrastructure_test.py
@@ -4,12 +4,11 @@ from os import listdir
 
 def run(hardware_dict, result_dict):
     """
-    check that the SD Card is initialized and test the logging infrastructure
-    log multiple files' worth of content, and check that the correct filenames
-    and correct number of files were created
+    If the SD card has been properly initialized, log 1800 characters
+    100 characters at a time, with a max file size of 1000 characters.
+    Make sure that 2 files have been created as a result of these logs.
     """
 
-    # check that the SD card is initialized
     if not hardware_dict["SDcard"]:
         result_dict["LoggingInfrastructure_Test"] = (
             "Cannot test Logging Infrastructure; no SD Card detected", None)
@@ -19,30 +18,29 @@ def run(hardware_dict, result_dict):
     clear_storage()
     print(f"/sd/ directory: {listdir('/sd/')}")
 
-    # log 1900 characters
+    # log 1800 characters
     for i in range(18):
         msg = 'x' * 100
         log(msg, 1000)
 
-    # check that log000.txt and log001.txt were created
     logfile1 = get_logfile_name(0)
     logfile2 = get_logfile_name(1)
-    logfile1_created = logfile1 in listdir("/sd/logs/")
-    logfile2_created = logfile2 in listdir("/sd/logs/")
 
     print(f"/sd/ directory: {listdir('/sd/')}")
     print(f"/sd/logs/ directory: {listdir('/sd/logs/')}")
 
-    # update result dictionary
+    # check that 2 files are created
+    logfile1_created = logfile1 in listdir("/sd/logs/")
+    logfile2_created = logfile2 in listdir("/sd/logs/")
     if logfile1_created and logfile2_created:
         result_string = """Log and new log functions are working;
 files were created successfully."""
-        result_dict["LoggingInfrastructure_Test"] = (result_string, True)
-        print(result_string)
     else:
         result_string = "New log function not working."
-        result_dict["LoggingInfrastructure_Test"] = (result_string, False)
-        print(result_string)
 
+    print(result_string)
+    result_dict["LoggingInfrastructure_Test"] = (
+        result_string, logfile1_created and logfile2_created)
     print("Logging Infrastructure Test complete.\n")
+
     clear_storage()
