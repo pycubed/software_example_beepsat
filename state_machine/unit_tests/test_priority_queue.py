e87d77ec6b03ce89b5b2b24881e6990e94e2e656
thetazero <lesha.seletskiy@gmail.com>
2022-08-25 11:47:06 -0400

    Lint

--- state_machine/unit_tests/test_priority_queue.py
+++ state_machine/unit_tests/test_priority_queue.py
@@ -16,7 +16,7 @@ def _isHeap(arr, i, n):
     # If an internal node and is greater
     # than its children, and same is
     # recursively true for the children
-    if(arr[i] >= arr[2 * i + 1] and
+    if (arr[i] >= arr[2 * i + 1] and
        arr[i] >= arr[2 * i + 2] and
        _isHeap(arr, 2 * i + 1, n) and
        _isHeap(arr, 2 * i + 2, n)):
