f4e579ac7f3acae141273b60e9ac343ca7d85e54
thetazero <lesha.seletskiy@gmail.com>
2022-09-03 14:14:55 -0400

    Document time_out.sh

--- .github/workflows/scripts/time_out.sh
+++ .github/workflows/scripts/time_out.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Script that runs an already compiled instance of the flight software, 
+# and then returns exit code 0, or 1 depending on if the flight software
+# ran successfully for 5 seconds.
+
 cd state_machine
 timeout 5s sh run.sh
 if [ $? == "124" ]; then
