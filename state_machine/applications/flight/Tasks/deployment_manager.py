16da8c3cb027acda9d43ea28d0a4ba6c2b27b74f
thetazero <lesha.seletskiy@gmail.com>
2022-11-16 14:19:12 -0500

    Update deployment code

--- state_machine/applications/flight/Tasks/deployment_manager.py
+++ state_machine/applications/flight/Tasks/deployment_manager.py
@@ -1,25 +1,35 @@
 from lib.template_task import Task
 from pycubed import cubesat
 from state_machine import state_machine
+import time
 
+
+INITIAL_BURN_DELAY = 5 * 60
+BURN_INTERVAL = 24 * 60 * 60
 
 class deployment_manager(Task):
     name = 'depoloyment_manager'
     color = 'blue'
 
     rgb_on = False
+    start_time = time.time()
+    last_burn = float('inf')
 
     async def main_task(self):
         if (cubesat.f_contact):
             self.debug('Contact with ground station has been already established, switching to Normal mode')
             state_machine.switch_to('Normal')
-        elif (cubesat.f_burn):
-            self.debug('Already burned, switching to Normal mode')
-            state_machine.switch_to('Normal')
-        else:
-            if await cubesat.burn(duration=15):
+        elif self.should_burn():
+            self.last_burn = time.time()
+            if await cubesat.burn(duration=10, duty_cycle=0.2):
                 cubesat.f_burn = True
                 self.debug('Successfully burned')
             else:
-                # Consider panic mode
                 self.debug('Unsuccessful burn')
+
+    def should_burn(self):
+        if (time.time() - self.start_time) > INITIAL_BURN_DELAY:  # first burn
+            return True
+        elif (time.time() - self.last_burn) > BURN_INTERVAL:  # other burns
+            return True
+        return False
